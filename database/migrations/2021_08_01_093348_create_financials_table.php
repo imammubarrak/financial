2366677262b8375208295a407f1029c5653417ad
imammubarrak <imam.mubarrak@gmail.com>
2021-08-02 15:22:02 +0700

    first commit

---
+++ database/migrations/2021_08_01_093348_create_financials_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFinancialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('financials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('business_id');
+            $table->foreignId('source_id');
+            $table->integer('min_budget');
+            $table->integer('max_budget');
+            $table->integer('est_return');
+            $table->string('status');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('financials');
+    }
+}
